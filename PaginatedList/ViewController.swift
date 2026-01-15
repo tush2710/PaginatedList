afc31c7639dbcbfacb76badb160c6dcbf60adf96
tmz2792 <139164822+tush2710@users.noreply.github.com>
2026-01-15 11:42:48 +0530

    Initial Commit

---
+++ PaginatedList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaginatedList
+//
+//  Created by Tushar Zade on 15/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
