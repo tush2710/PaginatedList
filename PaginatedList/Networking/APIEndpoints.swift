3b274e96d9a41af0a0be49837147e65fd83dcc58
Tushar Zade <tushar_zade@hotmail.com>
2026-01-16 01:33:32 +0530

    Add API Endpoint

---
+++ PaginatedList/Networking/APIEndpoints.swift
@@ -0,0 +1,10 @@
+//
+//  APIEndpoints.swift
+//  PaginatedList
+//
+//  Created by Tushar Zade on 15/01/26.
+//
+
+struct APIEndpoints{
+    static let getProducts = "https://api.escuelajs.co/api/v1/products"
+}
